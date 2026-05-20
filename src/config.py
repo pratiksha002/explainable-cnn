e32291a47e0d9113265e1b449def279e3d830e12
pratiksha002 <020606pratiksha@gmail.com>
2026-05-20 09:55:37 +0530

    Added configurations

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, "data", "raw")
 TRAIN_DIR = os.path.join(DATA_DIR, "seg_train", "seg_train")
-VAL_DIR = os.path.join(DATA_DIR, "seg_test",  "seg_test")
+##VAL_DIR = os.path.join(DATA_DIR, "seg_test",  "seg_test")
 
 OUTPUT_DIR      = os.path.join(BASE_DIR, "outputs")
 MODEL_SAVE_DIR  = os.path.join(OUTPUT_DIR, "models")
